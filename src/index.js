bff9dc5c8c7983d4de94312442ff82ff78cb8cd0
Francois Pakkanen <97538687+FrankPak@users.noreply.github.com>
2023-07-18 17:53:16 +0000

    Update 57 files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import "./styles.css";
+//let map = L.map("map").setView([61.06, 28.09], 13);
+
+/*let osm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  minZoom: -3,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+*/
+const fetchData = async () => {
+  const url =
+    "https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=tilastointialueet:kunta4500k&outputFormat=json&srsName=EPSG:4326";
+  const geoData = await fetch(url);
+  const geoJson = await geoData.json();
+
+  initMap(geoJson);
+};
+
+const initMap = (data) => {
+  let map = L.map("map", {
+    minZoom: -3,
+  });
+
+  let geoJson = L.geoJSON(data, {
+    weight: 2,
+  }).addTo(map);
+
+  let osm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+  map.fitBounds(geoJson.getBounds());
+};
+
+fetchData();
